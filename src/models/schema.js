626d4122fdf01512befb2ea14b528b83586159ad
Luke <llogan382@gmail.com>
2022-01-14 15:24:26 -0500

    Updates

--- src/models/schema.js
+++ src/models/schema.js
@@ -145,9 +145,6 @@ export const schema = {
                             {
                                 "allow": "public",
                                 "operations": [
-                                    "create",
-                                    "update",
-                                    "delete",
                                     "read"
                                 ]
                             }
